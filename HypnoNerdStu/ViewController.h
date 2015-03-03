22d7402f3fb1765b8eb42b541d5bd29eed9c8846
Stuart Dear <stuart.dear@intermarkgroup.com>
2015-03-03 04:55:08 -0600

    Initial Commit

---
+++ HypnoNerdStu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerdStu
+//
+//  Created by Stuart Dear on 3/3/15.
+//  Copyright (c) 2015 Big Nerd Ranch Stu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HypnoNerdStu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HypnoNerdStu
+//
+//  Created by Stuart Dear on 3/3/15.
+//  Copyright (c) 2015 Big Nerd Ranch Stu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
